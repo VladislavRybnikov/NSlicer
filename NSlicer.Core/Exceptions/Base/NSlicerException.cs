8133174e4edac4200fad1d3ef2b80f983480c79f
Vladyslav Rybnikov <vladyslav_rybnikov@epam.com>
2018-11-01 22:51:04 +0200

    added base Exceptions

---
+++ NSlicer.Core/Exceptions/Base/NSlicerException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace NSlicer.Core.Exceptions.Base
+{
+    public abstract class NSlicerException : Exception
+    {
+        public NSlicerException()
+        {
+        }
+
+        public NSlicerException(string message) : base(message)
+        {
+        }
+
+        public NSlicerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected NSlicerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ NSlicer.Core/Exceptions/MapperPropertyBindingException.cs
@@ -0,0 +1,27 @@
+﻿using NSlicer.Core.Exceptions.Base;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace NSlicer.Core.Exceptions
+{
+    public class MapperPropertyBindingException : NSlicerException
+    {
+        public MapperPropertyBindingException()
+        {
+        }
+
+        public MapperPropertyBindingException(string message) : base(message)
+        {
+        }
+
+        public MapperPropertyBindingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public MapperPropertyBindingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
